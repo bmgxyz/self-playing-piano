a773cd967bd1c184c9f3afa59dd6cb9196d9115c
Bradley Gannon <bradley@bradleygannon.com>
2025-04-30 18:05:51 -0400

    Switch to I2C on subcontroller

---
+++ avr-pwm/main.c
@@ -0,0 +1,73 @@
+#include <avr/io.h>
+
+extern void pwm(uint8_t *table_b, uint8_t *table_d);
+
+// libc zeros these arrays automatically
+static uint8_t table_b[127];
+static uint8_t table_d[127];
+
+enum state {
+    idle, start, key, velocity
+};
+
+void update_pwm(uint8_t key_idx, uint8_t key_vel) {
+    uint8_t *table_to_update = key_idx < 6 ? table_b : table_d;
+    for (int i = 0; i < 127; i++) {
+        if (key_vel > i) {
+            table_to_update[i] &= !(1 << key_idx);
+        } else {
+            table_to_update[i] |= 1 << key_idx;
+        }
+    }
+}
+
+int main(void) {
+    DDRB = 0b00111111;
+    DDRD = 0b00011111;
+    // I2C address is equal to the low nibble of PINC shifted left by one
+    TWAR = (PINC & 0b1111) << 1;
+    // configure I2C for slave receiver mode
+    TWCR = 0b01000100;
+
+    enum state s = idle;
+    uint8_t key_idx = 0;
+    uint8_t key_vel = 0;
+
+    while (1) {
+        // poll I2C
+        if (TWCR & (1 << TWINT)) {
+            switch (TWSR) {
+                case 0x60:
+                    s = start;
+                    break;
+                case 0x80:
+                    if (s == start) {
+                        if (TWDR > 11) {
+                            key_idx = 0;
+                            s = idle;
+                        } else {
+                            key_idx = TWDR;
+                            s = key;
+                        }
+                    } else if (s == key) {
+                        if (TWDR > 127) {
+                            key_idx = 0;
+                            key_vel = 0;
+                            s = idle;
+                        } else {
+                            s = velocity;
+                            key_vel = TWDR;
+                        }
+                    }
+                    break;
+                case 0xa0:
+                    if (s == velocity) {
+                        update_pwm(key_idx, key_vel);
+                        s = idle;
+                    }
+            }
+            TWCR |= (1 << TWINT);
+        }
+        pwm(table_b, table_d);
+    }
+}
